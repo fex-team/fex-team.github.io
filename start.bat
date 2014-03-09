249df704e8ee63dd888d0fe3161a2343d056d5a0
zenany <anemone928@gmail.com>
2014-03-09 17:44:54 +0800

    add support for windows

---
+++ start.bat
@@ -0,0 +1,31 @@
+:: by niuyao, start fex bolg, you can use : start.bat your_jekyll_home
+
+@echo off
+IF "%1"=="" (
+	echo 'please use as : start.bat your_jekyll_home !'
+	goto:eof
+)
+
+IF "%JEKYLL_HOME%" equ "" (
+	:: add jelly to env path and start fex blog...'
+	goto:set_path_and_start_blog
+) ELSE (
+	:: 'try to start fex blog...'
+	goto:start_fex_blog
+)
+
+:set_path_and_start_blog
+echo 'set jekyll env...'
+SET JEKYLL_HOME=%1
+SET PATH=%PATH%;%JEKYLL_HOME%\ruby\bin;%JEKYLL_HOME%\devkit\bin;%JEKYLL_HOME%\git\bin;%JEKYLL_HOME%\Python\App;%JEKYLL_HOME%\devkit\mingw\bin;%JEKYLL_HOME%\curl\bin
+goto:start_fex_blog
+
+:start_fex_blog
+echo 'start fex blog...'
+:: echo jekyll home : %JEKYLL_HOME%
+:: echo path : %PATH%
+@echo on
+jekyll serve -w
+pause
+
+
