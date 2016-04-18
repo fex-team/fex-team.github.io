e66d2dc03d5a6afde9de3e171c5990bf02b5ef59
hefangshi <hefangshi@gmail.com>
2016-04-18 12:00:43 +0800

    release fex-weekly 04.18

--- autolink.js
+++ autolink.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
-var fileName = './_posts/2016-02-29-fex-weekly-29.md';
+var fileName = process.argv[2];
 var content = fs.readFileSync(fileName).toString();
-content = content.replace(/([^\(])\n?((https?|ftp|mms):\/\/([A-z0-9]+[_\-]?[A-z0-9]+\.)*[A-z0-9]+\-?[A-z0-9]+\.[A-z]{2,}(\/.*)*\/?)/img, function (m, $1, $2) {
-    return $1 + '\r\n[' + $2 + '](' + $2 + ')\r\n';
+content = content.replace(/^(\*\*.*\*\*)$/img, '$1  ');
+content = content.replace(/([^\(])((https?|ftp|mms):\/\/([A-z0-9]+[_\-]?[A-z0-9]+\.)*[A-z0-9]+\-?[A-z0-9]+\.[A-z]{2,}(\/.*)*\/?)/img, function (m, $1, $2) {
+    return $1 + $2 + '  ';
 });
 fs.writeFileSync(fileName, content);
