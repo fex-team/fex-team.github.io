b7cddae50242e612860907cc2ce76f186c48e832
hefangshi <hefangshi@gmail.com>
2016-08-01 12:42:01 +0800

    update ruby version and gemfile

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-gem install jekyll jekyll-paginate redcarpet kramdown
-jekyll build
+bundle install
+
+bundle exec jekyll build
 
 tree ./_site
 
